144c63988e16a809f7d92000722fdd71d18cb3f7
Vladimir Starkov <iamstarkov@gmail.com>
2015-09-03 03:26:36 +0200

    fix deps issues

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,5 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
