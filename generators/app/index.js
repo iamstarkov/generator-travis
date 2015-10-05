b0c6722067e12b704b4ce5b891de8460136a059d
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-05 16:23:43 +0200

    support extend existing travis config

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,12 +1,24 @@
 'use strict';
 var yeoman = require('yeoman-generator');
+var yaml = require('yamljs');
+var deepAssign = require('deep-assign');
 
 module.exports = yeoman.generators.Base.extend({
   writing: {
     app: function () {
-      this.fs.copy(
-        this.templatePath('travisyml'),
-        this.destinationPath('.travis.yml')
+      var currentConfigFile = this.destinationPath('.travis.yml');
+      var configFile = this.templatePath('travisyml');
+
+      // Re-read the content at this point because a composed generator might modify it.
+      var currentConfig = this.fs.exists(currentConfigFile) ? yaml.parse(this.fs.read(currentConfigFile)) : {};
+      var config = yaml.parse(this.fs.read(configFile));
+
+      // Let's extend package.json so we're not overwriting user previous fields
+      var resultConfig = deepAssign({}, currentConfig, config);
+
+      this.fs.write(
+        this.destinationPath('.travis.yml'),
+        yaml.stringify(resultConfig, 2, 2)
       );
     },
   },
