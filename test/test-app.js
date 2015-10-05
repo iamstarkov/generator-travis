66ce51c2cdb0cc87ac484544f510666c837c31a2
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-10-05 20:05:00 +0200

    add tests for extending existing config and using config from options

--- test/test-app.js
+++ test/test-app.js
@@ -3,16 +3,44 @@
 var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
+var yaml = require('yamljs');
+
+var existingConfig = {
+  language: 'node_js',
+  node_js: [ 'stable' ],
+  before_script: ['bower install'],
+};
+var yamlExistingConfig = yaml.stringify(existingConfig, 3, 2)
 
 describe('travis:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
+      .withOptions({
+        config: { after_script: ['npm run coveralls'] }
+      })
+      .on('ready', function (gen) {
+        gen.fs.write(gen.destinationPath('.travis.yml'), yamlExistingConfig);
+      }.bind(this))
       .on('end', done);
   });
 
-  it('creates files', function () {
+  it('creates config', function () {
     assert.file([
       '.travis.yml'
     ]);
+  });
+
+  it('extends existing config', function () {
+    assert.fileContent(
+      '.travis.yml',
+      /bower install/
+    );
+  });
+
+  it('uses config from options', function () {
+    assert.fileContent(
+      '.travis.yml',
+      /npm run coveralls/
+    );
   });
 });
